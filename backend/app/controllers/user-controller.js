050ff50953d3ed54c0b05e95eaad740733a28129
fvnny-code <f.codecco.grando@gmail.com>
2022-01-25 11:27:56 +0100

    config user-controllers

--- backend/app/controllers/user-controller.js
+++ backend/app/controllers/user-controller.js
@@ -70,29 +70,91 @@ exports.login = (req, res, next) => {
 };
 
 // Middleware to delete a user account
-exports.deleteUser = (req, res, next) =>{
-    User.findOne({ where: {id: req.params.id} })
-    .then(user =>{
-        if(user.avatar != null){
-            const filename = user.avatar.split('/images/')[1];
-            fs.unlink(`images/${filename}`, ()=> {
-                User.deleteOne({where: {id: req.params.id} })
-                .then(()=>
-                    res.status(200).json({message:"Le compte Utilisateur a été supprimé."})
-                )
-                .catch (
-                    (error) => {res.status(404).json({error})}
-                )
-            })
-        } else {
-            User.deleteOne({where: {id: req.params.id} })
-            res.status(200).json({message: "Le compte utilisateur a été supprimé."})
-        }
+exports.deleteUser = (req, res, next) => {
+  User.findOne({ where: { id: req.params.id } })
+    .then((user) => {
+      if (user.avatar != null) {
+        const filename = user.avatar.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+          User.deleteOne({ where: { id: req.params.id } })
+            .then(() =>
+              res
+                .status(200)
+                .json({ message: "Le compte Utilisateur a été supprimé." })
+            )
+            .catch((error) => {
+              res.status(404).json({ error });
+            });
+        });
+      } else {
+        User.deleteOne({ where: { id: req.params.id } });
+        res
+          .status(200)
+          .json({ message: "Le compte utilisateur a été supprimé." });
+      }
     })
-    .catch(
-        (error) => res.status(500).json({error})
-    );
+    .catch((error) => res.status(500).json({ error }));
 };
 
-// Middleware to modify a user account (l 116)
+// Middleware to modify a user account
+exports.modifyUser = (req, res, next) => {
+  User.findOne({ where: { id: req.params.id } })
+    .then((user) => {
+      if (user.avatar !== null) {
+        let img = user.avatar.split("/images/")[1];
+        fs.unlink("images/" + img, () => {
+          User.update(
+            {
+              avatar:
+                req.protocol +
+                "://" +
+                req.get("host") +
+                "/images/" +
+                req.file.filename,
+            },
+            { where: { id: req.params.id } }
+          )
+            .then(() => {
+              res.status(201).json({ message: "Avatar mis à jour" });
+            })
+            .catch((error) => {
+              res.status(404).json({ error });
+            });
+        });
+      } else {
+        User.update(
+          {
+            avatar:
+              req.protocol +
+              "://" +
+              req.get("host") +
+              "/images/" +
+              req.file.filename,
+          },
+          { where: { id: req.params.id } }
+        );
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
+// Middleware to get a user account by its ID
+exports.getOneUser = (req, res, next) => {
+  User.findOne({ where: { id: req.params.id } });
+  then((user) => {
+    res.status(200).json(user);
+  }).catch((error) => {
+    res.status(404).json({ error });
+  });
+};
+
+// Middleware to get the list of all users accounts
+exports.getAllUsers = (req, res, next) => {
+  User.findAll({ attributes: ["id", "username", "avatar"] })
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
