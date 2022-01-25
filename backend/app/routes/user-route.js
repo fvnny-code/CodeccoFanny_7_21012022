b749b6cac5209ce7f9e3f23ec1e3c7ae9ccb5b0c
fvnny-code <f.codecco.grando@gmail.com>
2022-01-25 13:32:45 +0100

    config post-controller.js

--- backend/app/routes/user-route.js
+++ backend/app/routes/user-route.js
@@ -13,15 +13,16 @@ const passwordValidation = require ('../middleware/password');
 
 //CRUD//
 //Route to create a user (post)
-
+router.post('/signup', multer, passwordValidation, userController.signup);
 // Route to user login (post)
-
+router.post('/login', userController.login);
 // Route to delete a user (delete)
-
+router.delete('/:id', auth, userController.deleteUser);
 // Route to get a user by its id (get)
-
+router.get('/:id', auth, userController.getOneUser);
 // Route to  get all users (get)
-
+router.get('/', auth, userController.getAllUsers);
 // Route du modify a user (put)
+router.put('/:id', auth, multer, userController.modifyUser);
 
 module.exports = router;
