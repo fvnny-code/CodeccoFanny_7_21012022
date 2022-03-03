532ef21c1c99968c5c3bf4bbd3eeca5f92c293b9
fvnny-code <f.codecco.grando@gmail.com>
2022-03-03 17:45:03 +0100

    ajoute des routes : admin + post + user

---
+++ backend/routes/postRoute.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const apiRouter = express.Router();
+const auth = require('../middlewares/auth');
+const multer = require('../middlewares/multer-config');
+
+const postCtrl = require('../controllers/postCtrl')
+
+try{
+
+//Posts
+apiRouter.get();
+apiRouter.post();
+apiRouter.put();
+apiRouter.delete();
+//likes
+apiRouter.get();
+apiRouter.post();
+//Comments
+apiRouter.get();
+apiRouter.post();
+apiRouter.put();
+apiRouter.delete();
+
+
+} catch(error){
+    console.log(error);
+}
+
+apiRouter.get('/', postCtrl.getAllPosts);
+apiRouter.get('/:id', postCtrl.getOnePost);
+apiRouter.delete('/:id', postCtrl.deleteOnePost);
+apiRouter.put('/:id', postCtrl.updatePost);
+
+
+
+module.exports = apiRouter;
