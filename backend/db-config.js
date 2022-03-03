1f22c9f05be85abe94017333b480027c2b108207
fvnny-code <f.codecco.grando@gmail.com>
2022-03-03 16:33:26 +0100

    config connexion à la base de données

---
+++ backend/db-config.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+// configuration de la connexion à la DB
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password : 'root',
+    database : 'groupomania2',
+    port : 8889
+  });
+  
+  // connexion à la DB
+  module.exports.getDB =function(){
+    return db
+  }
+  
+  // déconnexion à la DB
+  module.exports.closeDB =function(){
+    db.end();
+  }
