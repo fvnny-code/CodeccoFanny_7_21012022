3c56c34ac83745583922d34542adfb7b2ce829b0
fvnny-code <f.codecco.grando@gmail.com>
2022-03-03 17:42:04 +0100

    ajoutdes middlewares auth et multer-config

---
+++ backend/middlewares/multer-config.js
@@ -0,0 +1,25 @@
+// Import of MULTER to manage images.
+const multer = require("multer");
+const path = require('path');
+
+// List of accepted images formats (MIME_TYPES)
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+
+// Storage location an file name
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    let name = file.originalname.split(".")[0];
+    name = name.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+module.exports = multer({ storage }).single("image");
