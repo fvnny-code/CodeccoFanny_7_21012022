11c8c10d09eb3c58b60f5d03d8cc0c8582a39fc9
fvnny-code <f.codecco.grando@gmail.com>
2022-03-17 10:39:44 +0100

    src/components/ FeedView : <style>

--- groupomania-frontend/src/components/FeedView.vue
+++ groupomania-frontend/src/components/FeedView.vue
@@ -3,14 +3,16 @@
     <h1>The wall</h1>
     <button v-on:click="getAllPosts()">Afficher les posts</button>
     <ul>
-      <li v-for="post in posts" v-bind:key="post.id">{{ post.title}} {{ post.content }}</li>
+      <li v-for="post in posts" v-bind:key="post.id">
+        {{ post.title }} {{ post.content }}
+      </li>
     </ul>
   </div>
 </template>
 <script>
 import axios from "axios";
 export default {
-  el: ".feed__container",  
+  el: ".feed__container",
   name: "TheFeed",
   data() {
     return {
@@ -22,8 +24,31 @@ export default {
       axios
         .get("http://localhost:3000/api/post")
         .then((response) => (this.posts = response.data))
-        .catch((error) => {console.log(error)});
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
 </script>
+<style scoped>
+.feed__container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 80%;
+  margin: 0 auto;
+  padding: 2rem;
+
+}
+
+h1, button {
+  width: fit-content;
+  margin: 1rem auto;
+}
+button:hover {
+  transform: scale(1.2);
+  transition: 0.6s;
+  cursor: pointer;
+}
+</style>
