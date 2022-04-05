ac201bb9ac1822a71a7cd6a8b76cb54bcc17090f
fvnny-code <f.codecco.grando@gmail.com>
2022-04-05 20:14:39 +0200

    front/src/components/TheHeader : navigation responsive avec menu burger = ok

--- groupomania-frontend/src/components/FeedView.vue
+++ groupomania-frontend/src/components/FeedView.vue
@@ -3,12 +3,9 @@
     <h2 class="feed__title">Le Mur</h2>
     <div class="container__allPosts">
       <button class="button" @click="displayPostForm">Créer un post</button>
+
       <!-- tous les posts -->
-      <div
-        class="card__post"
-        v-for="(post, index) in allPosts"
-        :key="index"
-      >
+      <div class="card__post" v-for="(post, index) in allPosts" :key="index">
         <h3 class="post__title">{{ post.title }}</h3>
         <p class="post__subtitle">
           Par {{ post.userName }}, le {{ dateFormat(post.date_creation) }}
@@ -18,10 +15,51 @@
         </p>
       </div>
     </div>
+<!-- essai de fenêtre modale : non conluant -->
+
+    <!-- <label>
+      <span class="button showForm"> Créer un post </span>
+      <input type="checkbox" />
+      <div class="confirm-modal">
+        <form action="#do-something" method="get">
+          <div class="form-row">
+            <label for="title">Titre : </label>
+            <input
+              type="title"
+              id="title"
+              name="post_title"
+              rows="8"
+              cols="30"
+              wrap="on"
+              autofocus
+              required
+            />
+            <span class="required"> * Ce champs est requis</span>
+            <label for="post-content">Que voulez-vous partager ?</label>
+            <textarea
+              id="content"
+              name="content"
+              placeholder="Dites quelque chose..."
+              rows="5"
+              cols="50"
+              maxlength="500"
+              autofocus
+              required
+            >
+            </textarea>
+            <span class="required"> * Ce champs est requis</span>
+          </div>
+          <div class="form-row">
+            <button class="btn-success" @click="sendPost">Poster</button>
+          </div>
+        </form>
+      </div>
+    </label> -->
   </div>
 </template>
 <script>
 import axios from "axios";
+// import PostForm from "../components/PostForm.vue";
 export default {
   name: "TheFeed",
   data() {
@@ -41,6 +79,7 @@ export default {
       form: true,
     };
   },
+  // components: { PostForm },
   created() {
     this.getAllUsers();
   },
@@ -49,8 +88,8 @@ export default {
       axios
         .get("http://localhost:3000/api/auth", {
           headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.token,
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.token,
           },
         })
         .then((response) => {
@@ -73,12 +112,36 @@ export default {
     displayPostForm(){
       this.$router.push('/home/feed/post')
     }
+    // avec fenêtre modale pour formulaire
+    // sendPost() {
+    //   this.dataPostS = JSON.stringify(this.dataPost);
+    //   console.log(this.dataPostS);
+    //   axios.post("http://localhost:3000/api/post/", this.dataPost, {
+    //     headers: {
+    //       "Content-type": "application/json",
+    //       Authorization: "Bearer " + localStorage.token,
+    //     },
+    //   })
+    //   .then((response)=>{
+    //       console.log(response.data)
+    //       let rep = response.data;
+    //       this.message = rep.message;
+    //       this.msg = true;
+    //       this.form= false;
+    //       this.$router.push('/home')
+    //   })
+    //   .catch((error)=>{
+    //       console.log(error);
+    //       this.message = error;
+    //       this.msg = true
+    //   });
+    // },
   },
   mounted() {
     this.userId = localStorage.userId;
     axios
       .get("http://localhost:3000/api/post", {
-        headers: { Authorization: 'Bearer '+ localStorage.token },
+        headers: { Authorization: "Bearer " + localStorage.token },
       })
       .then((response) => {
         console.log(response.data);
@@ -91,7 +154,7 @@ export default {
   },
 };
 </script>
-<style scoped>
+<style lang="scss">
 .feed__container {
   display: flex;
   flex-direction: column;
@@ -99,7 +162,7 @@ export default {
   gap: 1rem;
   width: 100%;
   height: 100vh;
-  margin: 0 auto;
+  margin: 5rem auto;
   padding: 2rem;
 }
 .container__allPosts {
@@ -107,7 +170,6 @@ export default {
   flex-direction: column;
   gap: 1rem;
 }
-
 .card__post {
   border-radius: 0.5rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
@@ -119,21 +181,102 @@ export default {
   font-size: 0.8rem;
   font-style: italic;
 }
-
-button {
-  border: none;
-  margin: auto;
-  max-width: 100%;
-  min-width: 200px;
-  padding: 0.5rem;
-  border-radius: 0.5rem;
-  color: white;
-  background-color: #bbb;
-}
-button:hover {
-  transform: scale(1.2);
-  transition: 0.6s;
+// Modal //
+// input[type="checkbox"] {
+//   position: relative;
+//   width: 20px;
+//   top: -20px;
+//   left: 20px;
+//   display: none;
+// }
+.button {
+  transition: background-color 0.2s;
+  background-color: transparent;
+  color: #8a8a8a;
+ 
+  padding: .8rem;
   cursor: pointer;
-  background-color: teal;
+  border-radius: .5rem;
+  border: 1px solid #8a8a8a;
+  user-select: none;
+  display: inline-block;
+  top: 10px;
+  // width: 200px;
+   min-width: 10rem;
+  margin: 0 auto;
+  text-align: center;
+  &:hover {
+    color: #fafafa;
+    background-color: #8a8a8a;
+  }
+  &:active,
+  &:focus {
+    color: #fafafa;
+    background-color: #a0a0a0;
+  }
 }
+// button:hover {
+//   transform: scale(1.2);
+//   transition: 0.6s;
+//   cursor: pointer;
+//   background-color: teal;
+// }
+
+.confirm-modal {
+  display: block;
+  pointer-events: none;
+  transition: opacity 0.5s;
+  .btn-success {
+    width: auto;
+    max-width: auto;
+    margin: auto;
+  }
+  &::before {
+    content: "";
+    display: block;
+    background-color: rgba(0, 0, 0, 0.4);
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    min-height: 200px;
+    z-index: 99;
+    opacity: 1;
+    // pointer-events: all;
+  }
+  form {
+    position: absolute;
+    z-index: 99;
+    top: 50px;
+    transition: transform 0.75s;
+    max-width: 90%;
+    width: 450px;
+    background-color: #fafafa;
+    padding: 20px;
+    left: 50%;
+    transform: translate(-50%, 0);
+    box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.2);
+    color: #080808;
+    border-radius: 5px;
+    pointer-events: none;
+  }
+}
+input:not(:checked) + .confirm-modal {
+  &,
+  & * {
+    opacity: 0;
+  }
+  &::before {
+    height: 0;
+    width: 0;
+  }
+}
+input:not(:checked) + .confirm-modal form {
+  transform: translate(-50%, 100px);
+}
+.confirm-modal button {
+  pointer-events: all;
+}
+
 </style>
